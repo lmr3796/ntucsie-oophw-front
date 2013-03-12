5305a44b2040bf7d8c1a31c897aa0120c9628d14
lmr3796 <lmr3796joseph@gmail.com>
2013-03-12 16:03:19 +0800

    Config omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.full_host = 'http://localhost:3000'
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :openid, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+end
