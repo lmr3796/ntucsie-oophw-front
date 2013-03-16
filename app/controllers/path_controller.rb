7823ebb22024df0c5355620b3ab5033aa5c3619e
lmr3796 <lmr3796joseph@gmail.com>
2013-03-16 22:27:24 +0800

    Proto type that does git clone

--- app/controllers/path_controller.rb
+++ app/controllers/path_controller.rb
@@ -6,13 +6,27 @@ class PathController < ApplicationController
       redirect_to '/auth/google' 
       return 
     end
-    if not /[brdf]([0-9]{8})/ =~ session[:email]
+    if not /@csie\.ntu\.edu\.tw/ =~ session[:email]
       render :text => session[:email] + " is invalid"
     end
     @email = session[:email]
+    @id = @email[0...@email.index('@')]
   end
 
   def git
-    render :text => "You're: " + @email + "<br/>You submitted \"" + params[:path] + "\""
+    @path = params[:path]
+    #render :text => "You're: " + @id + "<br/>You submitted \"" + @path + "\""
+    dest = "/tmp2/r01944027/#{@id}"
+    msg = `mkdir -p #{dest}`
+    if not $?.success?
+        render :text => "You're #{@id}, mkdir has failed, message is:<br/>#{msg}"
+        return
+    end
+    msg = `(cd #{dest}; git clone #{@path})`
+    if not $?.success?
+        render :text => "You're #{@id}, git clone failed, message is:<br/>#{msg}"
+        return
+    end
+    render :text => "You're #{@id}<br/>git clone succeed<br/>Go to #{request.host}:#{dest} and have a look!"
   end
 end
