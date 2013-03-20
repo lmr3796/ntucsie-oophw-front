4472942cdc1467871c931c5153529c3bf917777a
lmr3796 <lmr3796joseph@gmail.com>
2013-03-21 02:19:22 +0800

    Split git clone and git log into 2 steps to apply checks

--- app/controllers/path_controller.rb
+++ app/controllers/path_controller.rb
@@ -19,12 +19,17 @@ class PathController < ApplicationController
     dest = "/tmp2/oophw/#{@id}"
     @msg = `mkdir -p #{dest}`
     if not $?.success?
-        render :text => "You're #{@id}, mkdir has failed, message is:<br/>#{@msg}", :status => 500
+        render :text => "Hi #{@id}, mkdir has failed, message is:<br/>#{@msg}", :status => 500
         return
     end
-    @commit = `(cd #{dest} && git clone #{@repo} 2>&1 > /dev/null&& cd #{@repo.gsub(/\.git$/, "")[/\/.*$/][1..-1]} && git log -1)`
+    @msg = `(cd #{dest} && git clone #{@repo} 2>&1 > /dev/null)`
     if not $?.success?
-        render :text => "You're #{@id}, git clone failed", :status => 400
+        render :text => "Hi #{@id}, git clone failed", :status => 400
+        return
+    end
+    @commit = `(cd #{dest}/#{@repo.gsub(/\.git$/, "")[/\/.*$/][1..-1]} && git log -1)`
+    if not $?.success?
+        render :text => "Hi #{@id}, error fetching git repository info", :status => 500
         return
     end
     render :text => "Hi #{@id}, git clone succeeded.<br/>"+
