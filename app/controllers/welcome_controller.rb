defb94b2ac8de19dc1dd220a4ac5fad647552b20
lmr3796 <lmr3796joseph@gmail.com>
2013-03-27 22:37:55 +0800

    Add submission history.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,30 @@
+require 'grit'
+
 class WelcomeController < ApplicationController
   def index
     if session[:email]
       @email = session[:email]
       @id = @email[0...@email.index('@')]
+
+      @submissions = []
+
+      dest = "/tmp2/oophw#{@homework_number}/#{@id}"
+      Dir.foreach(dest) do |f|
+        n = f.to_i
+        if n.to_s == f
+          repo_dir = File.join(dest, f)
+          repo_uri = 'git://github.com/telgniw/some-repo.git'
+          repo = Grit::Repo.new(repo_dir) rescue NoSuchPathError
+          head = repo.commits.first
+          @submissions.push({
+            :version    => n,
+            :repo       => repo_uri,
+            :info       => head
+          })
+        end
+      end
+
+      @submissions.sort! { |x,y| y[:version] <=> x[:version] }
     end
   end
 end
