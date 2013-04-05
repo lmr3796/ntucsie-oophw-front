1397a87b8f811f6d71de94453741974b49e5641b
lmr3796 <lmr3796joseph@gmail.com>
2013-04-05 16:38:35 +0800

    Update admin list.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,9 @@ class ApplicationController < ActionController::Base
     @homework_number = 3
     @admins = [
       'htlin@csie.ntu.edu.tw',
-      'hsuantien@gmail.com',
       'r01922014@csie.ntu.edu.tw',
-      'ptt2panda@gmail.com',
       'r01922059@csie.ntu.edu.tw',
-      'celia781011@gmail.com',
       'r01944027@csie.ntu.edu.tw',
-      'lmr3796@gmail.com',
     ]
   end
 end
