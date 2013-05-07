3c1b8e2baa84f768dee72bf26ce38c4ac415b7db
lmr3796 <lmr3796joseph@gmail.com>
2013-05-07 20:24:01 +0800

    Change to homework 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def set_variables
-    @homework_number     = 3
-    @homework_deadline   = Time.new(2013, 4, 11, 12, 00)
+    @homework_number     = 4
+    @homework_deadline   = Time.new(2013, 5, 16, 12, 00)
 
     @admins = [
       'htlin@csie.ntu.edu.tw',
       'r01922014@csie.ntu.edu.tw',
       'r01922059@csie.ntu.edu.tw',
       'r01944027@csie.ntu.edu.tw',
+      'lmr3796joseph@gmail.com'
     ]
   end
 end
