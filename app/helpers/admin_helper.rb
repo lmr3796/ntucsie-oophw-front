afb348c0e9612a6d590c8b9c53485637740a9a21
lmr3796 <lmr3796joseph@gmail.com>
2013-05-15 17:20:04 +0800

    Update admin repo url.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,7 +2,7 @@ module AdminHelper
   def get_repo_url(repo)
     matched = /:(?<target>[^\.]+)\.git/.match(repo)
     if matched != nil
-      "http://bitbucket.org/#{matched[:target]}"
+      "http://bitbucket.org/#{matched[:target]}".downcase
     else
       '#'
     end
