2792397067d487832b075808b4e984d3e4d2942f
lmr3796 <lmr3796joseph@gmail.com>
2013-06-10 16:12:28 +0800

    Use localhost for testing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	OmniAuth.config.full_host = 'https://oophw.csie.org'
+	#OmniAuth.config.full_host = 'https://oophw.csie.org'
+	OmniAuth.config.full_host = 'http://localhost:3000'
 	provider :google_oauth2, '723504240573.apps.googleusercontent.com', '01uXTsiIRAWJPrAqqkStWa3j', { :scope => 'userinfo.email,userinfo.profile' }
 end
 
