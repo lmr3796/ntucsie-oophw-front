955af11a5458e44fcfd69b67d97f02ec5d5180ed
lmr3796 <lmr3796joseph@gmail.com>
2013-06-12 21:27:48 +0800

    Loading animation

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,7 +1,30 @@
 // Use ajax to refresh table
 function reload_history() {
+    // Add spin animation
+    var opt = {
+        lines: 13, // The number of lines to draw
+        length: 20, // The length of each line
+        width: 5, // The line thickness
+        radius: 20, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#666', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
+    };
+    var spinner = new Spinner(opt).spin($("#history-loading-spinner-holder").get(0));
+    $(".spinner").css("left", "50%").css("top", "95px")
+    $("#history-loading").fadeIn();
+
+    // Reload
     var hw_id = $('#hw_id').val();
-    // TODO: animation?
     $.ajax({
         url: "history/" + hw_id,
     }).done(function(res) {
@@ -38,6 +61,9 @@ function reload_history() {
         }
     }).fail(function() {
         console.log("No history get");
+    }).complete(function() {
+        $("#history-loading").fadeOut();
+        $("#history-loading .spinner").remove();
     });
 };
 
